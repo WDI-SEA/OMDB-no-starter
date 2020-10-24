0dd7a938e8297b72aba1a561fceee6084671c657
“camillebrown” <camillebrown11@gmail.com>
2020-10-24 14:16:00 -0700

    added style.css and faves

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express()
 const ejsLayouts = require('express-ejs-layouts')
 app.set('view engine','ejs')
 app.use(ejsLayouts)
+app.use(express.static('public'))
+const db = require('./models')
 
-app.use(express.urlencoded({extended: false}))
 app.use('/movies', require('./controllers/movies'))
-
 
 app.get('/', (req, res)=>{
     res.render('home.ejs')
 })
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('the 8000 port is on')
 })
